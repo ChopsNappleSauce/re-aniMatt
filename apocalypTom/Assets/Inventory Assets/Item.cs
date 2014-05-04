22a181a245d85e34882d06975452a94647da65b7
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-05-04 04:34:45 -0400

    Basic inv, stick pick up

---
+++ apocalypTom/Assets/Inventory Assets/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+	public string itemName;
+	public int itemID;
+	public string itemDesc;
+	public Texture2D itemIcon;
+	public int itemPower;
+	public int itemSpeed;
+	public ItemType itemType;
+
+	public enum ItemType {
+		Weapon,
+		Tool,
+		Resource,
+		Consumable
+	}
+}
--- apocalypTom/Assets/Player Assets/Tom.cs
+++ apocalypTom/Assets/Player Assets/Tom.cs
@@ -9,12 +9,15 @@ public class Tom : MonoBehaviour {
 	public int mobCap = 100;
 	public float zombieRate = 2f;
 	public float resourceRate = 2f;
+	public int invRows = 5;
+	public float grabRadius = 3f;
 
 	public Transform plainZombie;
 	public Transform forestZombie;
 	public Transform desertZombie;
 
 	public Transform stick;
+	public Texture invStick;
 
 	public Rigidbody projectile;
 
@@ -28,12 +31,19 @@ public class Tom : MonoBehaviour {
 	private int time = 0;
 	private int counter = 0;
 	private int kills = 0;
+	private Vector2[,] inv;
 
 	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		inv = new Vector2[invRows, invRows];
+	}
+
+	public Vector2[,] getInventory()
+	{
+		return inv;
 	}
 
 	public void takeDamage (float damage) {
@@ -142,76 +152,137 @@ public class Tom : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		//gui scrGUI =;
+		//GUITexture guiTexture = GetComponent
 
 		if (health > 0) 
 		{
-						if (UnityEngine.Random.Range (0, 1000) < (zombieRate * 10)) {		// 2% chance to spawn a zombie per frame.
-								trySpawnZombie ();
-						}
+			if (UnityEngine.Random.Range (0, 1000) < (zombieRate * 10)) {		// 2% chance to spawn a zombie per frame.
+				trySpawnZombie ();
+			}
 
 			if (UnityEngine.Random.Range (0, 1000) < (resourceRate * 10)) {		// 40% chance to spawn a zombie per frame.
 				trySpawnResource ();
 			}
 
-						float xDistance = 0;
-						float yDistance = 0;
+			float xDistance = 0;
+			float yDistance = 0;
 
-						if (Input.GetAxis ("Horizontal") > 0) {
-								xDistance = 1;
-						} else if (Input.GetAxis ("Horizontal") < 0) {
-								xDistance = -1;
-						}
-		
-						if (Input.GetAxis ("Vertical") > 0) {
-								yDistance = 1;
-						} else if (Input.GetAxis ("Vertical") < 0) {
-								yDistance = -1;
-						}
-
-						//I didn't know if there was a built in time function so I just came up with this
-						counter = counter + 1;
-						if (counter >= 58) {
-								time = time + 1;
-								counter = 0;
-						}
-						if (Input.GetKey (KeyCode.X)) {
-								transform.Translate (0f, 0f, moveSpeed * Time.deltaTime);
-						} else if (Input.GetKey (KeyCode.Z)) {
-								transform.Translate (0f, 0f, -moveSpeed * Time.deltaTime);
-						}
-
-						float angle = Mathf.Atan2 (Input.mousePosition.y - Screen.height / 2, Input.mousePosition.x - Screen.width / 2) * Mathf.Rad2Deg;
-						Vector3 currAngles = transform.eulerAngles;
-						Quaternion target = Quaternion.Euler (currAngles.x, 180f - angle, currAngles.z);
-						transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * 15f);
-
-						if (Input.GetMouseButtonDown (0)) {
-								if (ammo > 0) {
-										anim.Play ("Strike");
-
-										ammo--;
-										Rigidbody bullet = Instantiate (projectile, transform.position, transform.rotation) as Rigidbody;
-										bullet.velocity = transform.TransformDirection (new Vector3 (-bulletSpeed, 0, 0));
-
-
-								}
-						}
-		
-						if ((xDistance == 0) && (yDistance == 0)) {
-								anim.Play ("Idle2");
-						}
-
-						if ((xDistance != 0) || (yDistance != 0)) {
-								anim.Play ("Run2");
-						} else {
-								anim.Play ("Idle2");
-						}
-
-						// rotate
-						Vector3 moveVec = new Vector3 (xDistance, 0f, yDistance);
-						moveVec.Normalize ();
-						moveVec *= moveSpeed * Time.deltaTime;
-						transform.Translate (moveVec, Space.World);
+			if (Input.GetAxis ("Horizontal") > 0) {
+					xDistance = 1;
+			} else if (Input.GetAxis ("Horizontal") < 0) {
+					xDistance = -1;
 			}
+
+			if (Input.GetAxis ("Vertical") > 0) {
+					yDistance = 1;
+			} else if (Input.GetAxis ("Vertical") < 0) {
+					yDistance = -1;
+			}
+
+			//I didn't know if there was a built in time function so I just came up with this
+			counter = counter + 1;
+			if (counter >= 58) {
+					time = time + 1;
+					counter = 0;
+			}
+
+			if (Input.GetKey (KeyCode.X)) {
+					transform.Translate (0f, 0f, moveSpeed * Time.deltaTime);
+			} else if (Input.GetKey (KeyCode.Z)) {
+					transform.Translate (0f, 0f, -moveSpeed * Time.deltaTime);
+			}
+
+			//Space to grab resources
+			if(Input.GetKey(KeyCode.Space))
+			{
+				//Get all sticks and pick up ones close enough
+				GameObject[] sticks = GameObject.FindGameObjectsWithTag("stick");
+				foreach(GameObject stick in sticks)
+				{
+					float xSDif = stick.transform.position.x - this.transform.position.x;
+					float ySDif = stick.transform.position.z - this.transform.position.z;
+					float sDist = Mathf.Sqrt(Mathf.Pow(xSDif, 2f) + Mathf.Pow(ySDif, 2f));
+					if(sDist < grabRadius)
+					{
+						bool foundSpot = false;
+						for(int i = 0; i < invRows; i++)
+						{
+							for(int j = 0; j < invRows; j++)
+							{
+								//Debug.Log("[" + i + "," + j + "]:" + inv[i,j].x);
+								if(inv[i,j].x < 2)
+								{
+									inv[i,j].x = 1;
+									inv[i,j].y += 1;
+									foundSpot = true;
+									break;
+								}
+							}
+						}
+
+						if(foundSpot)
+						{
+							Debug.Log("Open Inv Spot");
+						}
+						else
+						{
+							Debug.Log("No Open Inv Spaces");
+						}
+
+						Destroy(stick);
+					}
+				}
+			}
+
+			float angle = Mathf.Atan2 (Input.mousePosition.y - Screen.height / 2, Input.mousePosition.x - Screen.width / 2) * Mathf.Rad2Deg;
+			Vector3 currAngles = transform.eulerAngles;
+			Quaternion target = Quaternion.Euler (currAngles.x, 180f - angle, currAngles.z);
+			transform.rotation = Quaternion.Slerp (transform.rotation, target, Time.deltaTime * 15f);
+
+			if (Input.GetMouseButtonDown (0)) {
+					if (ammo > 0) {
+							anim.Play ("Strike");
+
+							ammo--;
+							Rigidbody bullet = Instantiate (projectile, transform.position, transform.rotation) as Rigidbody;
+							bullet.velocity = transform.TransformDirection (new Vector3 (-bulletSpeed, 0, 0));
+
+
+					}
+				//GameObject button = GameObject.Find("inv0:0");
+				//GUITexture buttonTexture = button.GetComponent<GUITexture> ();
+				//buttonTexture.texture = invStick;
+			}
+
+			if ((xDistance == 0) && (yDistance == 0)) {
+					anim.Play ("Idle2");
+			}
+
+			if ((xDistance != 0) || (yDistance != 0)) {
+					anim.Play ("Run2");
+			} else {
+					anim.Play ("Idle2");
+			}
+
+			// rotate
+			Vector3 moveVec = new Vector3 (xDistance, 0f, yDistance);
+			moveVec.Normalize ();
+			moveVec *= moveSpeed * Time.deltaTime;
+			transform.Translate (moveVec, Space.World);
+
+			/*
+			string invStr = "[";
+			for(int i = 0; i < invRows; i++)
+			{
+				for(int j = 0; j < invRows; j++)
+				{
+					invStr += "(" + inv[i,j].x + "," + inv[i,j].y + ")";
+				}
+				invStr += "\n";
+			}
+			Debug.Log(invStr + "]");
+			*/
+		}
 	}
 }
